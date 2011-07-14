0e8a3668dba5db863ba5039840a358f58cce8f0c
Patrick Ewing <patrickhenryewing@gmail.com>
2011-07-14 14:34:42 -0400

    Add skeleton files

--- lib/storying/story.rb
+++ lib/storying/story.rb
@@ -1,6 +1,6 @@
 module Storying
   class Story
-    attr_reader :setting, :hero, :supporting_character, :conflict
+    attr_reader :setting, :hero, :supporting_character, :conflict, :villain
 
     def initialize
       @setting = Storying.settings.random
@@ -8,6 +8,11 @@ module Storying
 
       @hero = Hero.new(:story => self)
       @supporting_character = SupportingCharacter.new(:story => self)
+    end
+
+    # TODO: not yet implemented
+    def plot_twist
+      # character = [hero, supporting_character, villain].compact.random
     end
 
   end
