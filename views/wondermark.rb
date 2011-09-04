b32d3fd9fcc7bec483919131d9d1b617c8114826
Patrick Ewing <patrickhenryewing@gmail.com>
2011-09-04 13:47:18 -0700

    Wondermarks genre-fiction generator, ported to Storying code

--- views/wondermark.rb
+++ views/wondermark.rb
@@ -1,11 +1,21 @@
 module Storying
   module Views
     class Wondermark < StoryView
-      
-      def method_missing(m)
-        wondermarks[m].random
+
+      def self.pass_through(*methods)
+        methods.each do |m|
+          define_method m do
+            wondermarks[m.to_s].random
+          end
+        end
       end
-      
+
+      pass_through :location_adj, :location_noun, :protagonist, :discovery,
+                   :adversary, :assistant, :inventory, :conflict, :title_adj, :title_noun
+
+      # def wonda
+      #   wondermarks["title_adj"].random.inspect
+      # end
     end
   end
 end
